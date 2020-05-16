448f0804833bf23e7a5c390f06d23efd9b766ea5
Iqbal Fauzi <iqbalfawz@gmail.com>
2020-05-17 01:54:32 +0700

    Add object formatter

--- modules/lib-address/config.php
+++ modules/lib-address/config.php
@@ -2,7 +2,7 @@
 
 return [
     '__name' => 'lib-address',
-    '__version' => '0.3.0',
+    '__version' => '0.3.1',
     '__git' => 'git@github.com:getmim/lib-address.git',
     '__license' => 'MIT',
     '__author' => [
@@ -17,6 +17,9 @@ return [
         'required' => [
             [
                 'lib-model' => NULL
+            ],
+            [
+                'lib-formatter' => NULL
             ]
         ],
         'optional' => []
@@ -43,6 +46,139 @@ return [
                 'addr-district' => 'LibAddress\\Library\\Filter',
                 'addr-village' => 'LibAddress\\Library\\Filter',
                 'addr-zip' => 'LibAddress\\Library\\Filter'
+            ]
+        ]
+    ],
+    'libFormatter' => [
+        'formats' => [
+            'addr-country' => [
+                'id' => [
+                    'type' => 'number'
+                ],
+                'name' => [
+                    'type' => 'text'
+                ],
+                'updated' => [
+                    'type' => 'date'
+                ],
+                'created' => [
+                    'type' => 'date'
+                ]
+            ],
+            'addr-state' => [
+                'id' => [
+                    'type' => 'number'
+                ],
+                'country' => [
+                    'type' => 'object',
+                    'model' => [
+                        'name' => 'LibAddress\\Model\\AddrCountry',
+                        'field' => 'id',
+                        'type' => 'number'
+                    ],
+                    'format' => 'addr-country'
+                ],
+                'name' => [
+                    'type' => 'text'
+                ],
+                'updated' => [
+                    'type' => 'date'
+                ],
+                'created' => [
+                    'type' => 'date'
+                ]
+            ],
+            'addr-city' => [
+                'id' => [
+                    'type' => 'number'
+                ],
+                'state' => [
+                    'type' => 'object',
+                    'model' => [
+                        'name' => 'LibAddress\\Model\\AddrState',
+                        'field' => 'id',
+                        'type' => 'number'
+                    ],
+                    'format' => 'addr-state'
+                ],
+                'name' => [
+                    'type' => 'text'
+                ],
+                'updated' => [
+                    'type' => 'date'
+                ],
+                'created' => [
+                    'type' => 'date'
+                ]
+            ],
+            'addr-district' => [
+                'id' => [
+                    'type' => 'number'
+                ],
+                'city' => [
+                    'type' => 'object',
+                    'model' => [
+                        'name' => 'LibAddress\\Model\\AddrCity',
+                        'field' => 'id',
+                        'type' => 'number'
+                    ],
+                    'format' => 'addr-city'
+                ],
+                'name' => [
+                    'type' => 'text'
+                ],
+                'updated' => [
+                    'type' => 'date'
+                ],
+                'created' => [
+                    'type' => 'date'
+                ]
+            ],
+            'addr-village' => [
+                'id' => [
+                    'type' => 'number'
+                ],
+                'district' => [
+                    'type' => 'object',
+                    'model' => [
+                        'name' => 'LibAddress\\Model\\AddrDistrict',
+                        'field' => 'id',
+                        'type' => 'number'
+                    ],
+                    'format' => 'addr-district'
+                ],
+                'name' => [
+                    'type' => 'text'
+                ],
+                'updated' => [
+                    'type' => 'date'
+                ],
+                'created' => [
+                    'type' => 'date'
+                ]
+            ],
+            'addr-zipcode' => [
+                'id' => [
+                    'type' => 'number'
+                ],
+                'village' => [
+                    'type' => 'object',
+                    'model' => [
+                        'name' => 'LibAddress\\Model\\AddrVillage',
+                        'field' => 'id',
+                        'type' => 'number'
+                    ],
+                    'format' => 'addr-village'
+                ],
+                'name' => [
+                    'type' => 'text'
+                ],
+                'updated' => [
+                    'type' => 'date'
+                ],
+                'created' => [
+                    'type' => 'date'
+                ]
             ]
         ]
     ]
